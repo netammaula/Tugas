9a0e4a48fef2f6fcb45ad6b0dc547b9a1522cf97
netammaula <netammaula@Neta>
2018-08-02 09:42:45 +0700

    Menampilkan 10 Bilangan Ganjil Genap

---
+++ src/J08/Contoh3.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package J08;
+
+/**
+ *
+ * @author netammaula
+ */
+import java.util.Scanner;
+public class Contoh3 {
+    public static void main(String[] args)
+{    
+    //Menampilkan 10 bilangan ganjil pertama
+    int x = 1;
+    
+    for(x = 1; x <= 20; x+=2)
+{
+    System.out.print(x + " ");
+}
+    System.out.println("Bilangan Ganjil");
+    
+    //Menampilkan 8 bilangan genap pertama
+    int n = 2;
+    
+    for(n = 2; n <=16; n+=2)
+{
+    System.out.print(n + " ");
+}
+    System.out.println("Bilangan Genap");
+}
+}
+    
+    
