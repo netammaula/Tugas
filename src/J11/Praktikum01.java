a7ae1722cc4dc2280b44055a381e4e51b11241a0
netammaula <netammaula@Neta>
2018-08-10 08:19:21 +0700

    Initial Commit

---
+++ src/J11/Praktikum01.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package J11;
+
+/**
+ *
+ * @author netammaula
+ */
+public class Praktikum01 {
+    public static void main(String[] args)
+    {
+        String identitas = "nama lengkap / kelas / no";
+        System.out.println("Neta Minkhatul Maula / XRPL3 / 31 : "+ identitas);
+    }
+    
+}
