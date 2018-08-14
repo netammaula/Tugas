20132d7f2e4685e3615bd4190e8af88765da8f91
netammaula <netammaula@Neta>
2018-08-14 12:27:19 +0700

    Initial Commit

---
+++ src/J11/ConvertVocal2Angka.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package J11;
+
+/**
+ *
+ * @author netammaula
+ */
+public class ConvertVocal2Angka {
+    
+    private static void tampilJudul(String identitas)    {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+        
+        
+    }
+    public static void main(String[] args)  {
+       String identitas = "Neta Minkhatul Maula / XRPL3 / 31";
+        
+        tampilJudul(identitas);
+    }
+    
+}
