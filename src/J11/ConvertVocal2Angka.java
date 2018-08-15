bcf301e0705d9463f5c7255c90ee97a0b2a41032
netammaula <netammaula@Neta>
2018-08-15 12:32:34 +0700

    Initial Commit

--- src/J11/ConvertVocal2Angka.java
+++ src/J11/ConvertVocal2Angka.java
@@ -10,14 +10,23 @@ package J11;
  *
  * @author netammaula
  */
+import java.util.Scanner;
 public class ConvertVocal2Angka {
-    
     private static void tampilJudul(String identitas)    {
         System.out.println("Identitas : "+ identitas);
         
         System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
         
         
+    }
+    private static String tampilInput(){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Masukkan Kalimat: ");
+        String kalimat = scanner.nextLine();
+        System.out.println("Kalimat Asli : " + kalimat);
+        
+        return kalimat;
     }
     public static void main(String[] args)  {
        String identitas = "Neta Minkhatul Maula / XRPL3 / 31";
