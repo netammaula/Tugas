486572c8eff2381c99563049f2f64adf22cdb8b5
netammaula <netammaula@Neta>
2018-08-15 13:00:55 +0700

    Tambah Konversi

--- src/J11/ConvertVocal2Angka.java
+++ src/J11/ConvertVocal2Angka.java
@@ -17,11 +17,32 @@ public class ConvertVocal2Angka {
         
         System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
     }
+    private static String tampilInput() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Masukkan kalimat: ");
+        String kalimat = scanner.nextLine();
+        System.out.println("Kalimat Asli : " + kalimat);
+        
+        return kalimat;
+    }
+    private static String vocal2Angka(String kalimat)   {
+        char [][] arConvert =
+        {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+        
+        kalimat = kalimat.toLowerCase();
+        for(int i=0;i<arConvert.length;i++)
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+        
+        return kalimat;
+    }
     
     public static void main(String[] args)  {
        String identitas = "Neta Minkhatul Maula / XRPL3 / 31";
         
         tampilJudul(identitas);
+        String kalimat = tampilInput();
+        String convert = vocal2Angka(kalimat);
     }
     
 }
