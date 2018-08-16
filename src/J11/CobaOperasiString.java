18318b03e33da61b16fcfec6190fd5305a79cb8c
netammaula <netammaula@Neta>
2018-08-16 22:57:58 +0700

    Tugas Membuat Aplikasi

--- src/J11/CobaOperasiString.java
+++ src/J11/CobaOperasiString.java
@@ -47,5 +47,39 @@ public class CobaOperasiString {
         String s = "operasi";
         System.out.println("isi variable s : "+ s);
         System.out.println("perbandingan isi r dengan s (Case Sensitive) : " + r.compareTo(s));
+        
+         System.out.println("pada x terdapat huruf 'a' : "+ x. contains("a"));
+        
+        System.out.println("isi variable x besar semua : "+ x.toUpperCase());
+        System.out.println("isi variable x kecil semua : "+ x.toLowerCase());
+        
+        String t = "    operasi string";
+        System.out.println("isi variable t : \""+ t +"\"");
+        System.out.println("isi variable t tanpa spasi : \""+ t.trim() +"\"");
+        
+        System.out.println("gabungkan isi variable x dan r : "+ x.concat(r));
+        
+         System.out.println("karakter mulai posisi 3 di variable x : " + x.substring(3));
+        
+        System.out.println("karakter dari posisi 3-5 di variable x : " + x.substring(3,6));
+        
+        String tclean = t.trim();
+        System.out.println("isi variable tclean : "+ t.trim());
+        System.out.println("rubah 'i' jadi 'E' di variable tclean : " + tclean.replace("i","E"));
+        
+        String[] arrT = tclean.split(" ");
+        System.out.println("pecah kata di variable tclean : ");
+        for(int i=0;i<arrT.length;i++)
+            System.out.println(arrT[i]);
+        
+        String[] arrTr = tclean.split("r");
+        System.out.println("pecah berdasarkan huruf 'r' di variable tclean : ");
+        for(int i=0;i<arrTr.length;i++)
+            System.out.println(arrTr[i]);
+        
+        double d = 1545454000;
+        System.out.println(String.format("uang syaa %,.4f", d));
+        
+        System.out.println(String.format(Locale.forLanguageTag("in-ID"), "Uang saya Rp %,.2f", d));
     }
 }
---
+++ src/J11/Tugas.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package J11;
+
+/**
+ *
+ * @author netammaula
+ */
+import java.util.Locale;
+import java.util.Scanner;
+public class Tugas {
+    public static void main(String[] args) {
+        
+        String identitas = "Neta Minkhatul Maula / XRPL3 / 31";
+        System.out.println("Identitas : "+ identitas);
+       
+        String n = "Hello";
+        String m = "Kitty";
+        
+        String input;
+        Scanner baca = new Scanner(System.in);
+        
+        System.out.println("Masukkan Kata: ");
+        input = baca.nextLine();
+        
+        System.out.println("Hasil LowerCase: "+ input.toLowerCase());
+        
+        System.out.println("Hasil UpperCase: "+ input.toUpperCase());
+        
+        System.out.println(m.trim()+ "Hasil trim : \"" +"\"");
+        
+        
+        
+    
+    }
+        
+    }
+        
+        
+        
+        
+                
+                
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
